537c1ad3d28c25a0d517fa6cda42b74c3059a967
bradley <bradley@LAPTOP-QSKLTQDD.localdomain>
2023-03-28 16:45:41 +0900

    Create analyze_tlc_taxi_record_with_spark.py

---
+++ analyze_tlc_taxi_record_with_spark.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+
+conf = SparkConf().setAppName("PySpark - Read TLC Taxi Record")
+'''
+conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.0')
+conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider')
+conf.set('spark.hadoop.fs.s3a.access.key', '<access_key>')
+conf.set('spark.hadoop.fs.s3a.secret.key', '<secret_key>')
+conf.set('spark.hadoop.fs.s3a.session.token', '<toke>')
+'''
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
+file_path = 's3://tlc-taxi/2019/fhvhv_tripdata_2019-02.parquet'
+
+df = spark.read.format('parquet').load(file_path)
+df.show()
